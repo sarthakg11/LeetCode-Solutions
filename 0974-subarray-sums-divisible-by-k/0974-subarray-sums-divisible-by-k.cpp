3c81597759d18871f7a2e4caee4fd098b6724b52
sarthakg11 <127665657+sarthakg11@users.noreply.github.com>
2024-05-30 15:55:50 +0530

    Time: 48 ms (29.27%), Space: 35.2 MB (44.73%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0;i<n;i++){
             prefix_sum+=nums[i];
             rem=prefix_sum%k;
+            if(rem<0) rem=rem+k;
             if (rem==0) count++;
             if(mp.find(rem)!=mp.end()) count+=mp[rem];
             mp[rem]++;
